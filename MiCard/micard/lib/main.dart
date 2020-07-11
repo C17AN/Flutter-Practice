cf9be4308482a76150223bcb9756a45b4484ae92
c17an <kimchanmin1@gmail.com>
2020-07-12 00:47:11 +0900

    first Flutter repo commit

---
+++ MiCard/micard/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    ),
+  );
+}
